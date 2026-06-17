bbcc1053808ad0c9e78164f5dbdc2dfb9e176c68
V-Nessa-ai <V-Nessa-ai@users.noreply.github.com>
2026-06-17 12:06:11 +0000

    deploy: 96dfd28238802e1e34a2350f18dbb3becb806774

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2010465037"
+    serviceWorkerVersion: "200910960"
   }
 });
